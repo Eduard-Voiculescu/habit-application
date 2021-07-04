20281af9c95f777bf5a9e040190f55d76a2effa1
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2021-07-04 17:59:43 -0400

    Adding account attributes

--- src/main/java/com/demo/habit/model/Account.java
+++ src/main/java/com/demo/habit/model/Account.java
@@ -2,26 +2,35 @@ package com.demo.habit.model;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "profile")
-public class Profile {
+@Table(name = "account")
+public class Account {
 
     @Id
-    @Generated
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private String id;
 
     @NonNull
     @Column(name = "username")
     private String userName;
+
+    @NonNull
+    @Column(name = "firstname")
+    private String firstName;
+
+    @NonNull
+    @Column(name = "lastname")
+    private String lastName;
+
+    @NonNull
+    @Column(name = "email")
+    private String email;
 
     @NonNull
     @Column(name = "password")
